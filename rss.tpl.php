e678f2582c97fd6d35b73c18bf4369c907ab8021
Joonas From <joonas.from@gmail.com>
2016-12-31 11:13:15 +0200

    More escaping.

--- rss.tpl.php
+++ rss.tpl.php
@@ -3,7 +3,7 @@
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 	<title><?=htmlspecialchars($source->name);?></title>
-	<link><?=$source->link;?></link>
+	<link><?=htmlspecialchars($source->link);?></link>
 	<description><?=htmlspecialchars($source->description);?></description>
 	<lastBuildDate><?=date("r");?></lastBuildDate>
 	<atom:link href="<?=htmlspecialchars(RSS_URL . "/" . $source->name) . ".xml";?>" rel="self" type="application/rss+xml" />
@@ -22,7 +22,7 @@
 				]]>
 			</description>
 			<pubDate><?=date("r", ($item->seen->__toString() / 1000));?></pubDate>
-			<guid><?=$item->guid;?></guid>
+			<guid><?=htmlspecialchars($item->guid);?></guid>
 		</item>
 <?php endforeach;?>
 </channel>
